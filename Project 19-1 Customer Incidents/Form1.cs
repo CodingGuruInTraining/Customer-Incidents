d8f1b7e206017e657b5a764ea3bdad072116ed90
Mark Fox <Mark Fox@DESKTOP-MRLVIUI>
2016-12-14 15:23:01 -0600

    Add project files.

---
+++ Project 19-1 Customer Incidents/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_19_1_Customer_Incidents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.techSupport_DataDataSet);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'techSupport_DataDataSet.Incidents' table. You can move, or remove it, as needed.
+            this.incidentsTableAdapter.Fill(this.techSupport_DataDataSet.Incidents);
+            // TODO: This line of code loads data into the 'techSupport_DataDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.techSupport_DataDataSet.Customers);
+            
+        }
+
+        //private void fillByCustomerToolStripButton_Click(object sender, EventArgs e)
+        //{
+            //try
+            //{
+            //    this.incidentsTableAdapter.FillBy
+            //        this.ada
+            //    this.customersTableAdapter.fillByCustomer(this.techSupport_DataDataSet.Customers, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox.Text, typeof(int)))));
+            //    //this.incidentsTableAdapter
+            //}
+            //catch (System.Exception ex)
+            //{
+            //    System.Windows.Forms.MessageBox.Show(ex.Message);
+            //}
+
+        //}
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.incidentsTableAdapter.FillBy(this.techSupport_DataDataSet.Incidents, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox1.Text, typeof(int)))));
+                this.customersTableAdapter.FillByCust(this.techSupport_DataDataSet.Customers, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox2.Text, typeof(int)))));
+            // http://stackoverflow.com/questions/17672044/binding-multiple-datagridview
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+   
+
+        private void fillByCustToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.customersTableAdapter.FillByCust(this.techSupport_DataDataSet.Customers, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox2.Text, typeof(int)))));
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
