affe49ec2ed973bfeaef06bcfea163989c5ed41e
Mark Fox <Mark Fox@DESKTOP-MRLVIUI>
2016-12-14 20:41:47 -0600

    Started find customer form

---
+++ Project 19-1 Customer Incidents/frmFindCust.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_19_1_Customer_Incidents
+{
+    public partial class frmFindCust : Form
+    {
+        public frmFindCust()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.techSupport_DataDataSet);
+
+        }
+
+        private void frmFindCust_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'techSupport_DataDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.techSupport_DataDataSet.Customers);
+
+        }
+    }
+}
